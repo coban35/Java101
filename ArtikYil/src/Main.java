7ea835f77dada66cb86c2bf91006a3f90e7a07fa
FatinCoban <109141422+coban35@users.noreply.github.com>
2022-07-26 16:02:28 +0300

    Add files via upload

---
+++ ArtikYil/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int artikYil;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Lütfen Yıl Giriniz :");
+        artikYil= input.nextInt();
+        if (artikYil%100==0 || artikYil%4==0)
+        {
+            if (artikYil%400==0)
+            {
+                System.out.println(artikYil+": Bir Artik Yildir");
+            }
+          else  if (artikYil%4==0 && (artikYil%100)!=0)
+            {
+                System.out.println(artikYil+": Bir Artik Yildir");
+            }
+            else
+            {
+                System.out.println(artikYil+": Bir Artik Yil Degildir");
+            }
+        }
+        else {
+            System.out.println("Girdigin Sayi Artik Yil Degildir");
+        }
+    }
+}
