51dcea46912f75a8c4c795770fda351841253dae
FatinCoban <109141422+coban35@users.noreply.github.com>
2022-08-04 13:11:47 +0300

    Add files via upload

---
+++ DortVeBesinKuvvetleri/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int sayac;
+        Scanner input =new Scanner(System.in);
+        System.out.print("Lütfen Sayac Değerini Giriniz :");
+        sayac=input.nextInt();
+        for (int i=4; i<=5; i++)
+        {
+            for (int k=1; k<=sayac; k*=i)
+            {
+                System.out.println(k);
+            }
+            System.out.print("\n");
+        }
+    }
+}
