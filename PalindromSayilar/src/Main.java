8b0eff773499494c7b319d9efaccbe498e240fc7
FatihCoban <109141422+coban35@users.noreply.github.com>
2022-11-22 14:23:23 +0300

    Add files via upload

---
+++ PalindromSayilar/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+             palindrom palindrom=new palindrom();
+        System.out.println(palindrom.isPalindrom(4));
+    }
+}
+
---
+++ PalindromSayilar/src/palindrom.java
@@ -0,0 +1,20 @@
+public class palindrom {
+    static boolean isPalindrom(int number) {
+        int temp = number, lastNumber;
+        int newNumber = 0;
+
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            newNumber = (newNumber * 10) + lastNumber;
+
+
+            temp /= 10;
+        }
+
+        if (newNumber == number)
+            return true;
+        else
+            return false;
+
+    }
+}
